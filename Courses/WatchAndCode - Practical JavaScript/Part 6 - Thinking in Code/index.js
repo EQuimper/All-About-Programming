fad881c4bf16be14f14064c32a32ade964abbb3f
EQuimper <quimperemanuel@gmail.com>
2016-05-29 23:17:20 -0600

    Finish the part 6 about Thinking in Code

--- Courses/WatchAndCode - Practical JavaScript/Part 6 - Thinking in Code/index.js
+++ Courses/WatchAndCode - Practical JavaScript/Part 6 - Thinking in Code/index.js
@@ -1,4 +1,4 @@
-// Come from the part 4
+// Come from the part 5
 var todoList = {
 	todos: [],
 	displayTodos: function () {
@@ -37,59 +37,77 @@ var todoList = {
 		var todo = this.todos[pos];
 		todo.completed = !todo.completed;
 		this.displayTodos();
+	},
+	// Toggle All
+	toggleAll: function () {
+		var totalTodos = this.todos.length;
+		var completedTodos = 0;
+		// Get number of completed todos
+		for (var i = 0; i < totalTodos; i++) {
+			if (this.todos[i].completed === true) {
+				completedTodos++; // Add one to the completedTodos variable
+			}
+		}
+		// Case 1: If everything's true, make everything false.
+		if (completedTodos === totalTodos) {
+			// Make everything false
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = false;
+			}
+		} else { // Case 2: Otherwise, make everything true.
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = true;
+			}
+		}
+		this.displayTodos();
 	}
 };
 
 todoList.addTodo('banana');
+todoList.addTodo('orange');
 todoList.addTodo('apple');
 
-/* Result
- My todos:
- todoText: banana
- My todos:
- todoText: banana
- todoText: apple
- */
-
-todoList.displayTodos(); // If we comment out the two add mehod before we got the todos.length === 0
-/* Result
- Your todos list is empty!
- */
-
-// With the add of the if statement about the completed property
-/* Result
- My todos:
- ( ) banana
- My todos:
- ( ) banana
- ( ) apple
- My todos:
- ( ) banana
- ( ) apple
- */
-
+todoList.toggleCompleted(0);
 todoList.toggleCompleted(1);
+todoList.toggleCompleted(2);
+
+todoList.toggleAll();
 /* Result
  My todos:
  ( ) banana
  My todos:
  ( ) banana
- ( ) apple
+ ( ) orange
  My todos:
  ( ) banana
+ ( ) orange
  ( ) apple
  My todos:
- ( ) banana
+ (x) banana
+ ( ) orange
+ ( ) apple
+ My todos:
+ (x) banana
+ (x) orange
+ ( ) apple
+ My todos:
+ (x) banana
+ (x) orange
  (x) apple
+ My todos:
+ ( ) banana
+ ( ) orange
+ ( ) apple
  */
 
-todoList.addTodo('orange');
-todoList.addTodo('pineaple');
-todoList.toggleCompleted(2);
+todoList.toggleAll();
 /* Result
  My todos:
  ( ) banana
- (x) apple
+ ( ) orange
+ ( ) apple
+ My todos: // Everything now it's true
+ (x) banana
  (x) orange
- ( ) pineaple
+ (x) apple
  */
---
+++ Courses/WatchAndCode - Practical JavaScript/Part 6 - Thinking in Code/notes.md
@@ -0,0 +1,3 @@
+### What we gonna do in this part
+- .toggleAll: if everything's true, make everything false.
+- .toggleAll: otherwise, make everything true.
